c7e2250dc1a5dcc9860aa902db2792e5106fb518
Pei Cheng <duffy.cheng@gmail.com>
2018-06-24 14:39:15 +0800

    menu template and style

--- scripts/template.js
+++ scripts/template.js
@@ -1,4 +1,7 @@
 (async () => {
-    const footerTemplate = await templates.load('../templates/footer.html')
+    const footerTemplate = await templates.load('../templates/footer.html'),
+        menuTemplage = await templates.load('../templates/menu.html');
     document.querySelector('footer').appendChild(footerTemplate.content.cloneNode(true));
+    document.querySelector('.menu-content-top-nav').appendChild(menuTemplage.content.cloneNode(true));
+    document.querySelector('.menu-content-side-bar').appendChild(menuTemplage.content.cloneNode(true));
 })();
