91cd8648118c41c62da528bf8208175b579c7aa5
Pei Cheng <duffy.cheng@gmail.com>
2018-07-22 12:47:33 +0800

    靜態畫面調整

--- scripts/template.js
+++ scripts/template.js
@@ -2,6 +2,5 @@
     const footerTemplate = await templates.load('../templates/footer.html'),
         menuTemplage = await templates.load('../templates/menu.html');
     document.querySelector('footer').appendChild(footerTemplate.content.cloneNode(true));
-    document.querySelector('.menu-content-top-nav').appendChild(menuTemplage.content.cloneNode(true));
     document.querySelector('.menu-content-side-bar').appendChild(menuTemplage.content.cloneNode(true));
 })();
