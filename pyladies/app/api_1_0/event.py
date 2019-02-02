2295b28fcea67df404d8edbcabde6ff323375501
Huiyu Lee <iownthegame@fiiser.com>
2019-02-02 23:19:43 +0800

    add /events api with params: keyword, date, sort, order

--- pyladies/app/api_1_0/event.py
+++ pyladies/app/api_1_0/event.py
@@ -1,9 +1,12 @@
-from flask import current_app, jsonify
+from flask import current_app, jsonify, request
 
 from . import api
-from ..exceptions import OK
+from ..exceptions import (
+    OK, EVENTLIST_INVALID_KEYWORD, EVENTLIST_INVALID_DATE,
+    EVENTLIST_INVALID_SORT, EVENTLIST_INVALID_ORDER, EVENTLIST_ERROR)
 from ..sqldb import DBWrapper
-from ..utils import HashableDict
+from ..utils import HashableDict, validate_time_format
+from ..managers.event import Manager as EventManager
 
 
 @api.route("/event/<int:e_id>", methods=["GET"])
@@ -96,3 +99,58 @@ def get_event(e_id):
         }
 
         return jsonify(data=data, info=info)
+
+@api.route("/events", methods=["GET"])
+def list_events():
+     # ## event list constants
+    EVENTLIST_PARAM_KEYWORD_DEFAULT = ""
+    EVENTLIST_PARAM_KEYWORD_MAX_LEN = 30
+    EVENTLIST_PARAM_DATE_DEFAULT = ""
+    EVENTLIST_PARAM_SORT_DEFAULT = "date"
+    EVENTLIST_PARAM_SORT_OPTIONS = ["date"]
+    EVENTLIST_PARAM_ORDER_DEFAULT = "asc"
+    EVENTLIST_PARAM_ORDER_OPTIONS = ["asc", "desc"]
+
+    keyword = request.args.get("keyword", EVENTLIST_PARAM_KEYWORD_DEFAULT)
+    date = request.args.get("date", EVENTLIST_PARAM_DATE_DEFAULT)
+    sort = request.args.get("sort", EVENTLIST_PARAM_SORT_DEFAULT)
+    order = request.args.get("order", EVENTLIST_PARAM_ORDER_DEFAULT)
+
+    # ## validate request parameters
+    if len(keyword) > EVENTLIST_PARAM_KEYWORD_MAX_LEN:
+        raise EVENTLIST_INVALID_KEYWORD
+
+    if date:
+        try:
+            validate_time_format(date, '%Y-%m')
+        except ValueError:
+            raise EVENTLIST_INVALID_DATE
+
+    if not sort in EVENTLIST_PARAM_SORT_OPTIONS:
+        raise EVENTLIST_INVALID_SORT
+
+    if not order in EVENTLIST_PARAM_ORDER_OPTIONS:
+        raise EVENTLIST_INVALID_ORDER
+
+    # ## get event list from manager
+    em = EventManager()
+    try:
+        events = em.search_events(keyword, date, sort, order)
+    except Exception:
+        data = None
+        info = {
+            "code": EVENTLIST_ERROR.code,
+            "message": EVENTLIST_ERROR.message,
+        }
+        return jsonify(data=data, info=info)
+
+    data = {
+        "events": events,
+        "count": len(events),
+    }
+    info = {
+        "code": OK.code,
+        "message": OK.message
+    }
+
+    return jsonify(data=data, info=info)
