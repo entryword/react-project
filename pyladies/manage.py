ced367c8999c0e74051eec3a5dafb443df2f3204
rylie <rylie.chien@gmail.com>
2019-02-07 13:04:21 +0800

    add mock apply info cmd

--- pyladies/manage.py
+++ pyladies/manage.py
@@ -9,6 +9,7 @@ from app.managers.topic import Manager as TopicManager
 from app.managers.event import Manager as EventManager
 from app.managers.speaker import Manager as SpeakerManager
 from app.managers.place import Manager as PlaceManager
+from app.managers.apply import Manager as ApplyManager
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -147,6 +148,38 @@ def place(create, update_sn, delete_sn, f, ls):
 
     if ls:
         tm.list_places()
+
+@manager.option('-c', '--create', action='count', help='create apply info (must be used with -f)')
+@manager.option('-u', '--update', dest='update_sn', metavar='ID', type=int,
+                help='update apply info (must be used with -f)')
+@manager.option('-d', '--delete', dest='delete_sn', metavar='ID', type=int, help='delete apply info')
+@manager.option('-g', '--get', dest='get_sn', metavar='ID', type=int, help='get apply info')
+@manager.option('-f', '--file', dest='f', metavar='FILE')
+def apply(create, update_sn, delete_sn, get_sn, f):
+    tm = ApplyManager()
+    #filepath should be checked 
+    if create:
+        if not f:
+            print("Please specify the JSON file path by '-f FILE'.")
+        else:
+            tm.create_event_apply_info(f)
+            print("Create event apply info successfully.")
+
+    if update_sn:
+        if not f:
+            print("Please specify the JSON file path by '-f FILE'.")
+        else:
+            tm.update_event_apply_info(update_sn, f)
+            print("Update event (sn={}) apply info successfully.".format(update_sn))
+
+    if delete_sn:
+        tm.delete_event_apply_info(delete_sn)
+        print("Delete event (sn={}) apply info successfully.".format(delete_sn))
+
+    if get_sn:
+        event_apply_info = tm.get_event_apply_info(get_sn)
+        print(event_apply_info)
+
 
 
 if __name__ == '__main__':
