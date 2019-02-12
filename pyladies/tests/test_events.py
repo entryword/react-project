325c35420e7627bd8c00d68639420ac3425df2f4
Huiyu Lee <iownthegame@fiiser.com>
2019-02-13 00:56:06 +0800

    update test case for events w/ keyword & date

--- pyladies/tests/test_events.py
+++ pyladies/tests/test_events.py
@@ -123,7 +123,7 @@ class EventsTestCase(unittest.TestCase):
             event_basics = manager.search_event_basics(keyword, date)
             self.assertEquals(len(event_basics), 0)
 
-    def test_search_events_with_date_and_keyword(self):
+    def test_search_events_with_date(self):
         topics = [
             {
                 "name": "topic 1",
@@ -222,6 +222,85 @@ class EventsTestCase(unittest.TestCase):
             self.assertEquals(len(event_basics), 2)
             self.assertEquals(event_basics[0].event_info.title, "event 2")
             self.assertEquals(event_basics[1].event_info.title, "event 3")
+
+    def test_search_events_with_keywords_and_date(self):
+        topics = [
+            {
+                "name": "topic 1",
+                "desc": "this is topic 1",
+                "freq": 0,
+                "level": 0,
+                "host": 0,
+                "fields": [0],
+            },
+            {
+                "name": "topic 2",
+                "desc": "this is topic 2",
+                "freq": 0,
+                "level": 0,
+                "host": 0,
+                "fields": [0],
+            },
+        ]
+        place_info = {
+            "name": "國立台灣大學",
+            "addr": "台北市大安區羅斯福路四段1號",
+            "map": "",
+        }
+        event_basics = [
+            {
+                "topic_sn": 1,
+                "date": "2019-01-07",
+                "start_time": "10:00",
+                "end_time": "12:00",
+                "place_sn": 1,
+            },
+            {
+                "topic_sn": 1,
+                "date": "2019-02-03",
+                "start_time": "17:30",
+                "end_time": "20:00",
+                "place_sn": None,
+            },
+            {
+                "topic_sn": 2,
+                "date": "2019-02-17",
+                "start_time": "17:30",
+                "end_time": "20:00",
+                "place_sn": None,
+            },
+        ]
+        event_infos = [
+            {
+                "event_basic_sn": 1,
+                "title": "event 1",
+                "desc": "this is event 1",
+                "fields": [0, 1],
+            },
+            {
+                "event_basic_sn": 2,
+                "title": "event 2",
+                "desc": "this is event 2",
+                "fields": [0],
+            },
+            {
+                "event_basic_sn": 3,
+                "title": "event 3",
+                "desc": "this is event 3",
+                "fields": [0],
+            },
+        ]
+
+        # preparation
+        with DBWrapper(self.app.db.engine.url).session() as db_sess:
+            manager = self.app.db_api_class(db_sess)
+            for topic in topics:
+                manager.create_topic(topic, autocommit=True)
+            manager.create_place(place_info, autocommit=True)
+            for event_basic in event_basics:
+                manager.create_event_basic(event_basic, autocommit=True)
+            for event_info in event_infos:
+                manager.create_event_info(event_info, autocommit=True)
 
             # test date 2019-02 and keyword event 2
             keyword = 'event 2'
