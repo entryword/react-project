33c1c740a47e5e4322e7cab5eee71b010cd3e48a
CoCo Lin <coco_lin@trendmicro.com>
2019-02-16 14:25:00 +0800

    refactor event_apply api and unit test

--- pyladies/app/api_1_0/apply.py
+++ pyladies/app/api_1_0/apply.py
@@ -1,23 +1,32 @@
-from flask import current_app, jsonify
+from flask import jsonify
 
+from app.managers import apply
 from . import api
 from ..exceptions import OK
-from ..sqldb import DBWrapper
 
-@api.route("/apply/<int:e_sn>", methods=["GET"])
-def get_event_apply_info(e_sn):
-    with DBWrapper(current_app.db.engine.url).session() as db_sess:
-        manager = current_app.db_api_class(db_sess)
-        event_apply_info = manager.get_event_apply_info(e_sn)
 
-        data = {
-            "event_apply_info": event_apply_info
-        }
 
-        info = {
-            "code": OK.code,
-            "message": OK.message
-        }
+@api.route("/apply_info/<int:event_apply_sn>", methods=["GET"])
+def get_event_apply_info(event_apply_sn):
+    manager = apply.Manager()
+    event_apply_info = manager.get_event_apply_info(event_apply_sn)
 
-        return jsonify(data=data, info=info)
-        
+    info = {
+        "code": OK.code,
+        "message": OK.message
+    }
+
+    return jsonify(data=event_apply_info, info=info)
+
+
+@api.route("/event/<int:event_basic_sn>/apply_info", methods=["GET"])
+def get_event_apply_info_by_event_basic_sn(event_basic_sn):
+    manager = apply.Manager()
+    event_apply_info = manager.get_event_apply_info_by_event_basic_sn(event_basic_sn)
+
+    info = {
+        "code": OK.code,
+        "message": OK.message
+    }
+
+    return jsonify(data=event_apply_info, info=info)
--- pyladies/app/managers/apply.py
+++ pyladies/app/managers/apply.py
@@ -16,13 +16,31 @@ class Manager(BaseApplyManager):
     def get_event_apply_info_by_event_basic_sn(event_basic_sn):
         with DBWrapper(current_app.db.engine.url).session() as db_sess:
             manager = current_app.db_api_class(db_sess)
-            return manager.get_event_apply_by_event_basic_sn(event_basic_sn)
+            event_apply_info = manager.get_event_apply_by_event_basic_sn(event_basic_sn)
+            data = {
+                "event_basic_sn": event_apply_info.event_basic_sn,
+                "apply": event_apply_info.apply,
+                "start_time": event_apply_info.start_time,
+                "end_time": event_apply_info.end_time,
+                "limit": event_apply_info.limit,
+                "limit_desc": event_apply_info.limit_desc
+            }
+            return data
 
     @staticmethod
     def get_event_apply_info(event_apply_sn):
         with DBWrapper(current_app.db.engine.url).session() as db_sess:
             manager = current_app.db_api_class(db_sess)
-            return manager.get_event_apply(event_apply_sn)
+            event_apply_info = manager.get_event_apply(event_apply_sn)
+            data = {
+                "event_basic_sn": event_apply_info.event_basic_sn,
+                "apply": event_apply_info.apply,
+                "start_time": event_apply_info.start_time,
+                "end_time": event_apply_info.end_time,
+                "limit": event_apply_info.limit,
+                "limit_desc": event_apply_info.limit_desc
+            }
+            return data
 
     @staticmethod
     def update_event_apply_info(event_apply_sn, update_info):
