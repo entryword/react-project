89b70c542da87b96094b21fbeeb84253abaa5fbb
Pei Cheng <duffy.cheng@gmail.com>
2019-02-24 19:30:25 +0800

    解決 event list 日曆 view 日期無法更新問題，首頁event list 網址修正

--- frontend_react/src/CalendarToolbar.js
+++ frontend_react/src/CalendarToolbar.js
@@ -2,46 +2,20 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 class CalendarToolbar extends Component {
-    state = {
-        selectedDate: moment().format('YYYY-MM'),
-        prevDate: moment(moment().toDate())
-            .subtract(1, 'month')
-            .format('YYYY-MM'),
-        nextDate: moment(moment().toDate())
-            .add(1, 'month')
-            .format('YYYY-MM'),
-    };
-
     render() {
-        const { selectedDate, prevDate, nextDate } = this.state;
-        const { onNavigate } = this.props;
+        const { onNavigate, date } = this.props;
+        const selectedDate = moment(date).format('YYYY-MM');
+        const prevDate = moment(moment(date).toDate())
+            .subtract(1, 'month')
+            .format('YYYY-MM');
+        const nextDate = moment(moment(date).toDate())
+            .add(1, 'month')
+            .format('YYYY-MM');
         const onPrev = () => {
             onNavigate('PREV');
-            this.setState({
-                prevDate: moment(prevDate)
-                    .subtract(1, 'month')
-                    .format('YYYY-MM'),
-                nextDate: moment(nextDate)
-                    .subtract(1, 'month')
-                    .format('YYYY-MM'),
-                selectedDate: moment(selectedDate)
-                    .subtract(1, 'month')
-                    .format('YYYY-MM'),
-            });
         };
         const onNext = () => {
             onNavigate('NEXT');
-            this.setState({
-                prevDate: moment(prevDate)
-                    .add(1, 'month')
-                    .format('YYYY-MM'),
-                nextDate: moment(nextDate)
-                    .add(1, 'month')
-                    .format('YYYY-MM'),
-                selectedDate: moment(selectedDate)
-                    .add(1, 'month')
-                    .format('YYYY-MM'),
-            });
         };
         return (
             <div className="calendar-toolbar">
