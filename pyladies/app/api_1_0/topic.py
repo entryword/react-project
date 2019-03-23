37686d3f479db23e66eee07ae64a86374a166618
gvblack <gvblack8022@gmail.com>
2019-03-23 16:57:02 +0800

    remove pylint errror.

--- pyladies/app/api_1_0/topic.py
+++ pyladies/app/api_1_0/topic.py
@@ -1,10 +1,8 @@
-from flask import current_app, jsonify
-
+from flask import jsonify
+from app.managers.topic import Manager as TopicManager
 from . import api
 from ..exceptions import OK
-from ..sqldb import DBWrapper
-from ..utils import HashableDict
-from app.managers.topic import Manager as TopicManager
+
 
 @api.route("/topic/<int:t_id>", methods=["GET"])
 def get_topic(t_id):
