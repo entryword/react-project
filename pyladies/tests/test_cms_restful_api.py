f86cfeefb20dffdd03f863a01953f071c368562b
Maomao <fox91119@gmail.com>
2019-04-10 16:24:45 +0000

    Move test case to the right test file

--- pyladies/tests/test_cms_restful_api.py
+++ pyladies/tests/test_cms_restful_api.py
@@ -1,7 +1,95 @@
+# coding=UTF-8
+
+import json
+
 import pytest
 
 from app import create_app
 from app.sqldb import DBWrapper
+
+
+class TestCreateEvent:
+    def setup(self):
+        self.app = create_app('test')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.app.db.create_all()
+        self.test_client = self.app.test_client()
+
+    def teardown(self):
+        self.app.db.session.remove()
+        self.app.db.drop_all()
+        self.app_context.pop()
+
+    def test_success(self, topic_info, place_info):
+        speaker_info = {
+            "name": "speaker 1",
+            "photo": "https://pyladies.marsw.tw/img/speaker_1_photo.png",
+            "title": "Senior Engineer",
+            "major_related": True,
+            "intro": "",
+            "fields": [3],
+        }
+        assistant_info = {
+            "name": "speaker 2",
+            "photo": "https://pyladies.marsw.tw/img/speaker_2_photo.png",
+            "title": "Engineer",
+            "major_related": True,
+            "intro": "",
+            "fields": [3],
+        }
+
+        postdata = {
+            "data": {
+                "title": "XXXX",
+                "topic_id": None,
+                "start_date": "2019-03-09",
+                "start_time": "14:00",
+                "end_date": "2019-03-09",
+                "end_time": "17:00",
+                "place_id": None,
+                "desc": "XXXX",
+                "speaker_ids": [1],
+                "assistant_ids": [2],
+                "field_ids": [1, 2],
+            }
+        }
+        # preparation
+        with DBWrapper(self.app.db.engine.url).session() as db_sess:
+            manager = self.app.db_api_class(db_sess)
+            manager.create_topic(topic_info, autocommit=True)
+            manager.create_place(place_info, autocommit=True)
+            manager.create_speaker(speaker_info, autocommit=True)
+            manager.create_speaker(assistant_info, autocommit=True)
+
+            topic = manager.get_topic_by_name(topic_info["name"])
+            place = manager.get_place_by_name(place_info["name"])
+            postdata["data"]["topic_id"] = topic.sn
+            postdata["data"]["place_id"] = place.sn
+
+        # post test
+        rv = self.test_client.post(
+            "/cms/api/event",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(postdata),
+            content_type="application/json",
+        )
+        # api assertion
+        assert rv.status_code == 200
+        assert rv.json["info"]["code"] == 0
+        assert rv.json["data"]["id"] == 1
+        event_basic_sn = rv.json["data"]["id"]
+
+        # event assertion
+        with DBWrapper(self.app.db.engine.url).session() as db_sess:
+            manager = self.app.db_api_class(db_sess)
+            event_basic = manager.get_event_basic(event_basic_sn)
+            assert event_basic.topic.name == topic_info["name"]
+            assert event_basic.place.name == place_info["name"]
+            assert event_basic.place.map == place_info["map"]
+            assert event_basic.date == postdata["data"]["start_date"]
+            assert event_basic.start_time == postdata["data"]["start_time"]
+            assert event_basic.end_time == postdata["data"]["end_time"]
 
 
 class TestGetEvents:
