6cd03f7ec7a9b9db0b78e50d54481ffa32bab2e3
rylie <rylie.chien@gmail.com>
2019-04-28 13:24:41 -0700

    add cms get_places api and UT

---
+++ pyladies/app/cms/place.py
@@ -0,0 +1,28 @@
+from flask import current_app, jsonify
+
+from . import api
+from ..exceptions import OK
+from ..sqldb import DBWrapper
+
+
+@api.route("/places", methods=["GET"])
+def get_places():
+    with DBWrapper(current_app.db.engine.url).session() as db_sess:
+        manager = current_app.db_api_class(db_sess)
+        places = manager.get_places()
+
+        places_list = []
+        for place in places:
+            data = {
+                "addr": place.addr,
+                "id": place.sn,
+                "name": place.name
+            }
+            places_list.append(data)
+        data = places_list
+        info = {
+            "code": OK.code,
+            "message": OK.message
+        }
+
+        return jsonify(data=data, info=info)
