faa76aedcec9dc458e8e02179af35e0df6d3b196
Sonia Wu <sonia8715@gmail.com>
2019-05-01 19:59:20 +0800

    Create api for creating slide function

--- pyladies/app/managers/slide.py
+++ pyladies/app/managers/slide.py
@@ -20,4 +20,17 @@ class Manager(BaseSlideManager):
                 }
                 slide_data.append(data)
             return slide_data
-                
+
+    @staticmethod
+    def create_slide(info):
+        with DBWrapper(current_app.db.engine.url).session() as db_sess:
+            manager = current_app.db_api_class(db_sess)
+            slide = manager.create_slide_resource(info, autocommit=True)
+            return slide
+
+    @staticmethod
+    def get_slide(sn):
+        with DBWrapper(current_app.db.engine.url).session() as db_sess:
+            manager = current_app.db_api_class(db_sess)
+            slide = manager.get_slide_resource(sn)
+            return slide
