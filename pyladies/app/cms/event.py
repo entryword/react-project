cf408c5e8aeb6d883d893e3d8af0260de9d1dde9
Maomao <fox91119@gmail.com>
2019-06-10 13:00:05 +0000

    Allow speakers, assistants, apply_infos and slide_resources to be empty

--- pyladies/app/cms/event.py
+++ pyladies/app/cms/event.py
@@ -21,7 +21,7 @@ def get_events():
 
 @api.route("/event", methods=["POST"])
 @login_required
-def createEvent():
+def create_event():
     request_data = request.get_json()
 
     data = request_data["data"]
@@ -66,28 +66,35 @@ def get_event(e_id):
     }
 
     return jsonify(data=event_info, info=info)
-    
-    
+
+
 @api.route("/event/<int:e_id>", methods=["PUT"])
 @login_required
-def put_event(e_id):
+def update_event(e_id):
     request_data = request.get_json()
     data = request_data["data"]
     eventinfo = { "event_basic": {}, "event_info": {}, "apply_info": {}}
 
-    if data["topic_id"]: eventinfo["event_basic"]["topic_sn"] = data["topic_id"]
-    if data["start_date"]: eventinfo["event_basic"]["date"] = data["start_date"]
-    # TODO:waiting table schema and data["end_date"] will add here
-    if data["start_time"]: eventinfo["event_basic"]["start_time"] = data["start_time"]
-    if data["end_time"]: eventinfo["event_basic"]["end_time"] = data["end_time"]
-    if data["place_id"]: eventinfo["event_basic"]["place_sn"] = data["place_id"]
-    if data["title"]: eventinfo["event_info"]["title"] = data["title"]
-    if data["desc"]: eventinfo["event_info"]["desc"] = data["desc"]
-    if data["field_ids"]: eventinfo["event_info"]["fields"] = data["field_ids"]
-    if data["slide_resource_ids"]: eventinfo["event_info"]["slide_resource_sns"] = data["slide_resource_ids"]
-    if data["speaker_ids"]: eventinfo["event_info"]["speaker_sns"] = data["speaker_ids"]
-    if data["assistant_ids"]: eventinfo["event_info"]["assistant_sns"] = data["assistant_ids"]
-    if data["apply"]: eventinfo["apply_info"]["apply"] = data["apply"]
+    eventinfo = {
+        "event_basic": {
+            "topic_sn": data["topic_id"],
+            "date": data["start_date"],
+            "start_time": data["start_time"],
+            "end_time": data["end_time"],
+            "place_sn": data["place_id"]
+        },
+        "event_info": {
+            "title": data["title"],
+            "desc": data["desc"],
+            "fields": data["field_ids"],
+            "slide_resource_sns": data["slide_resource_ids"],
+            "speaker_sns": data["speaker_ids"],
+            "assistant_sns": data["assistant_ids"]
+        },
+        "apply_info": {
+            "apply": data["apply"]
+        }
+    }
 
     event_service = EventManager()
     event_service.update_event(e_id, eventinfo)
@@ -102,4 +109,3 @@ def put_event(e_id):
     }
 
     return jsonify(data=data, info=info)
-
