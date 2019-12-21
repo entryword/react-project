0e06e4be5780525a5544d4b92d91ef21d7aa1ead
Pei Cheng <duffy.cheng@gmail.com>
2019-12-21 10:17:30 +0800

    活動列表 年度下拉加上 2020

--- frontend_react/src/Year.js
+++ frontend_react/src/Year.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React from 'react';
 
 function Year(props) {
-  return (
-    <select
-      className="form-control search-year"
-      id="year"
-      value={props.value}
-      onChange={props.onChange}
-      onBlur={props.onBlur}
-    >
-      <option key="2015" value="2015">
-        2015
-      </option>
-      <option key="2016" value="2016">
-        2016
-      </option>
-      <option key="2017" value="2017">
-        2017
-      </option>
-      <option key="2018" value="2018">
-        2018
-      </option>
-      <option key="2019" value="2019">
-        2019
-      </option>
-    </select>
-  );
+    return (
+        <select
+            className="form-control search-year"
+            id="year"
+            value={props.value}
+            onChange={props.onChange}
+            onBlur={props.onBlur}>
+            <option key="2015" value="2015">
+                2015
+            </option>
+            <option key="2016" value="2016">
+                2016
+            </option>
+            <option key="2017" value="2017">
+                2017
+            </option>
+            <option key="2018" value="2018">
+                2018
+            </option>
+            <option key="2019" value="2019">
+                2019
+            </option>
+            <option key="2020" value="2020">
+                2020
+            </option>
+        </select>
+    );
 }
 
 export default Year;
