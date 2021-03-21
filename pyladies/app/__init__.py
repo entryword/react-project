321716488ed772ad112b6360993750144e6ee00a
Huiyu <iownthegame@gmail.com>
2021-03-21 22:39:29 +0100

    Add prompt to google auth url

--- pyladies/app/__init__.py
+++ pyladies/app/__init__.py
@@ -64,7 +64,7 @@ def create_app(config_name):
         access_token_url='https://accounts.google.com/o/oauth2/token',
         access_token_params=None,
         authorize_url='https://accounts.google.com/o/oauth2/auth',
-        authorize_params=None,
+        authorize_params={'prompt': 'select_account'},
         api_base_url='https://www.googleapis.com/oauth2/v1/',
         # userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
         client_kwargs={'scope': 'openid email profile'},
